2f24fa5ec8de0b52164474f932b242e0d9b19da8
ASkeTizy <artemnevmerz@gmail.com>
2022-05-19 21:33:13 +0300

    feat: add 4 task solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require('node:fs');
+const copyFile = require('node:fs/promises');
+const path = require('path');
+const DIR = './files';
+
+if (!fs.exists(__dirname, (e)=>{}) ){
+	fs.mkdir( path.join( __dirname, 'files-copy'), (err) => {});
+
+}
+	fs.readdir(__dirname + '/files', (err, files) => {				
+		files.forEach( file => {
+			fs.copyFile(__dirname + '/files/'+file, __dirname+'/files-copy/'+file, fs.constants.COPYFILE_FICLONE, 
+				(err)=>{
+					if (err) throw err;
+					console.log(file+' '+__dirname+'/files-copy/'+file+' '+fs.constants.COPYFILE_FICLONE)});
+		})
+	})
 
