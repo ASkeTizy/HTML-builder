81b8f814a115222262220c3d118417aec82f2889
ASkeTizy <45819392+ASkeTizy@users.noreply.github.com>
2022-05-25 22:57:11 +0300

    fix: folder creation

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,8 +5,15 @@ const DIR = './files';
 
 if (!fs.exists(__dirname, (e)=>{}) ){
 	fs.mkdir( path.join( __dirname, 'files-copy'), (err) => {});
-
-}
+fs.readdir(__dirname + '/files', (err, files) => {				
+		files.forEach( file => {
+			fs.copyFile(__dirname + '/files/'+file, __dirname+'/files-copy/'+file, fs.constants.COPYFILE_FICLONE, 
+				(err)=>{
+					if (err) throw err;
+					console.log(file+' '+__dirname+'/files-copy/'+file+' '+fs.constants.COPYFILE_FICLONE)});
+		})
+	})
+}else{
 	fs.readdir(__dirname + '/files', (err, files) => {				
 		files.forEach( file => {
 			fs.copyFile(__dirname + '/files/'+file, __dirname+'/files-copy/'+file, fs.constants.COPYFILE_FICLONE, 
@@ -15,4 +22,5 @@ if (!fs.exists(__dirname, (e)=>{}) ){
 					console.log(file+' '+__dirname+'/files-copy/'+file+' '+fs.constants.COPYFILE_FICLONE)});
 		})
 	})
-
+}
+	
