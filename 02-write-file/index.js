482ec14609d2653caccaaea5ae7467c9d241e520
ASkeTizy <45819392+ASkeTizy@users.noreply.github.com>
2022-05-26 08:45:42 +0300

    fix: file creation

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -9,43 +9,44 @@ const r = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
+ let writeStream = fs.createWriteStream('02-write-file/secret.txt');
  r.question("Enter your text here: ", function(answer) {
-	r.on('SIGINT',function(){
-					let writeStream = fs.createWriteStream('02-write-file/secret.txt');
-	  			writeStream.write(answer);
-	 				r.close();
-	  			console.log("End of file");
-	 			})
-			if (answer.includes('exit')){
-				let writeStream = fs.createWriteStream('02-write-file/secret.txt');
-				let ans = answer.replace('exit','');
-	  		writeStream.write(ans);
-	  		console.log("End of file");
-	   		r.close();
-				
-			}
-	 
-		
-	r.on('line',(input) =>{
-		answer += '\n'+ input;
-		r.resume();
+  
+  r.on('SIGINT',function(){
+         
+          writeStream.write(answer);
+          r.close();
+          console.log("End of file");
+        })
+      if (answer.includes('exit')){
 
- 		 r.on('SIGINT',function(){
-				let writeStream = fs.createWriteStream('02-write-file/secret.txt');
-  			writeStream.write(answer);
- 				r.close();
-  			console.log("End of file");
- 			})
-		if (answer.includes('exit')){
-			let writeStream = fs.createWriteStream('02-write-file/secret.txt');
-			let ans = answer.replace('exit','');
-  		writeStream.write(ans);
-  		console.log("End of file");
-   		r.close();
-			
-		}
- 	})
+        let ans = answer.replace('exit','');
+        writeStream.write(ans);
+        console.log("End of file");
+        r.close();
+        
+      }
+   
+    
+  r.on('line',(input) =>{
+    answer += '\n'+ input;
+    r.resume();
+
+     r.on('SIGINT',function(){
+        
+        writeStream.write(answer);
+        r.close();
+        console.log("End of file");
+      })
+    if (answer.includes('exit')){
+     
+      let ans = answer.replace('exit','');
+      writeStream.write(ans);
+      console.log("End of file");
+      r.close();
+      
+    }
+  })
 
 
 });
